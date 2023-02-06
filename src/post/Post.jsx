c77c9e42bb7eb0691027038ab630c1e23e590104
Kacper Najder <kacper.najder@post.local>
2023-02-06 12:02:17 +0000

    Added posts component, sidebar

---
+++ src/post/Post.jsx
@@ -0,0 +1,32 @@
+import "./post.css"
+
+export default function Post() {
+  return (
+    <div className="post">
+      <img src="https://images.pexels.com/photos/9803050/pexels-photo-9803050.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+      alt="" 
+      className="postImg"/>
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">
+              Music
+          </span>
+          <span className="postCat">
+              Life
+          </span>
+        </div>
+        <span className="postTitle">
+            Lorem ipsum dolor sit amet
+        </span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+      </p>
+    </div>
+  )
+}
